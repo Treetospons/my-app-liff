1e10418ee8948ff1ffe36228b3fd4a0126b84923
treetospon <treetospons@gmail.com>
2025-12-10 20:29:44 +0700

    add liff

--- app/rich-menu/page.tsx
+++ app/rich-menu/page.tsx
@@ -2,12 +2,31 @@
 
 import React, { useEffect } from 'react';
 import liff from '@line/liff';
+import { redirect, RedirectType } from 'next/navigation';
 
 const Page = () => {
     useEffect(() => {
         // This effect runs once when the component mounts
         console.log('Rich Menu Page loaded');
-        // liff.init({ liffId: 'YOUR_LIFF_ID' })
+
+        const liffinit = async () => {
+            const liffId = '2008669868-BqGZKXQK';
+            await liff.init({ liffId: liffId })
+            liff.ready.then(async () => {
+                if (!liff.isLoggedIn()) {
+                    liff.login();
+                }
+
+                const profile = await liff.getProfile();
+                alert('Hello, ' + profile.displayName);
+
+                redirect('/rich-menu/menu', RedirectType.push);
+            }).catch((err) => {
+                alert('LIFF initialization failed: ' + JSON.stringify(err));
+            })
+        }
+
+        liffinit()
     }, []);
 
     return (
